c0fb9214f26e1a56b3693255797f0ae2ebd8f1d9
Perryandr <81926324+Perryandr@users.noreply.github.com>
2023-04-18 22:41:52 -0700

    new Lines

--- Class 2 ops.py
+++ Class 2 ops.py
@@ -16,14 +16,32 @@ ip_address = "8.8.8.8"
 interval = 2 
 
 
+#!/usr/bin/env python3
+
+import os
+import datetime
+import time
+# destination IP to ping
+
+ip = '8.8.8.8'  
+
+
 
 while True:
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
-    response = os.system(f"ping -c 1 {ip_address} > /dev/null 2>&1")
-    status = "Network Active" if response == 0 else "Network Error"
-    print(f"{timestamp} {status} to {ip_address}")
-    time.sleep(interval)
-
+    # ping the IP and capture the return code
+    ret_code = os.system(f"ping -c 1 {ip} > /dev/null 2>&1")
+    
+    # evaluate the return code and assign status
+    if ret_code == 0:
+        status = 'Success'
+    else:
+        status = 'Failure'
+        
+    # print the status along with timestamp and destination IP
+    timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+    print(f"{timestamp} {status} to {ip}")
+    
+    time.sleep(2)  # wait for 2 seconds before next ping
 
 
 
