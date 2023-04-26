b8e3d3e40bd3de30b4c91241c49aa6b6d5ecebf1
Perryandr <81926324+Perryandr@users.noreply.github.com>
2023-04-26 00:17:08 -0700

    Updates to ops challenges 6 and 7.

--- Class 07.py
+++ Class 07.py
@@ -3,23 +3,71 @@
 
 # Script: Ops 401 Class 05 Ops challenge
 # Author: Andrew P.
-# Date of lastest revision: 04/24/2023
-# Purpose: Encrypting and decrypting a file with python.
+# Date of lastest revision: 04/26/2023
+# Purpose: Encrypting and decrypting a file and folders in a path with python.
+# Thanks to classmate Alex, instructor Alex for his demo and the introduction 
+# to python help tools and practice sites, and chat.gpt to help me fix any 
+# I messed up.
+
 
 # Import
 import os
+import shutil
+from cryptography.fernet import Fernet
+
+
+# Create a Fernet with key
+fernet = Fernet(key)
+
+# Folder walkthrough for recursive
+for root, dirs, files in os.walk(folder_path):
+    for file in files:
+        file_path = os.path.join(root, file)
+    
+
+    # Read the file
+    with open(file_path, "rb") as f:
+        file_contents = f.read()
+        
+    #encrypt file
+    encrypted_contents = fernet.encrypt(file_contents)
 
 
 
-# 
-#
-#
-#
-#
-#
+# Decrypt folders and files
+def decrypt_folder(folder_path, key):
+    # Create a Fernet instance with the key
+    fernet = Fernet(key)
+
+    # Walk through the folder tree recursively
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+
+            # Read the contents of the file
+            with open(file_path, "rb") as f:
+                file_contents = f.read()
+
+            # Decrypt the file contents
+            decrypted_contents = fernet.decrypt(file_contents)
+
+            # Write the decrypted contents back to the file
+            with open(file_path, "wb") as f:
+                f.write(decrypted_contents)
 
 
 
+
+
+if __name__ == "__main__":
+    # Generate a key
+    key = Fernet.generate_key()
+
+    # Encrypt a folder and its contents
+    encrypt_folder("/path/to/folder", key)
+
+    # Decrypt the encrypted folder and its contents
+    decrypt_folder("/path/to/folder", key)
 
 
 
--- Class op 06.py
+++ Class op 06.py
@@ -3,82 +3,105 @@
 
 # Script: Ops 401 Class 05 Ops challenge
 # Author: Andrew P.
-# Date of lastest revision: 04/24/2023
+# Date of lastest revision: 04/26/2023
 # Purpose: Encrypting and decrypting a file with python.
+# Thanks to classmate Alex, instructor Alex for his demo and the introduction 
+# to python help tools and practice sites, and chat.gpt to help me fix any 
+# I messed up.
+
+
 
 # Import
-pip3 install cryptography
-
-from cryptography.Fernet import Fernet
-Import os
+from cryptography.fernet import Fernet
+import os
 
 
 # Define key
 def write_key():
 
     key = Fernet.generate.key()
-    with open("key.key", "wb") as key_file
-    key.file.write(key)
+    with open("key.key", "wb") as key_file:
+     key.file.write(key)
+     
+key = Fernet.generate_key()
+
+# Fernet def
+fernet = Fernet(key)
+
+# Encryption
+def encrypt_file(key, filepath):
+    with open(filepath, 'rb') as f:
+        data = f.read()
+    fernet = Fernet(key)
+    encrypted = fernet.encrypt(data)
+    os.remove(filepath)
+    with open(filepath, 'wb') as f:
+        f.write(encrypted)
+
+def encrypt_string(key, plaintext):
+    fernet = Fernet(key)
+    encrypted = fernet.encrypt(plaintext.encode())
+    print(encrypted.decode())
+
+# Decryption
+def decrypt_file(key, filepath):
+    with open(filepath, 'rb') as f:
+        data = f.read()
+    fernet = Fernet(key)
+    decrypted = fernet.decrypt(data)
+    os.remove(filepath)
+    with open(filepath, 'wb') as f:
+        f.write(decrypted)
 
 
-
-# Function to Load Key
-def load_key():
-    
-    return open("key.key", "rb")
-
-
-# Encrypt
-f = Fernet(key)
-with open(filename, "rb") as file:
-    file_data = file.read()
-    # encrypts file
-    encrypted_data = f.encrypt(file_data)
-    
-
-with open(filename, "wb") as file:
-    file.write(encrypted_data)
-    
-    
-    
-# Encrypt a message    
-def encrypt_message(message, key):    
-    Encrypts a message
-    
-    f = Fernet(key)
-    
-    
-# prints the encrypted message
-print(f"Encrypted message(message, key)"):    
-    
+def decrypt_string(key, ciphertext):
+    fernet = Fernet(key)
+    decrypted = fernet.decrypt(ciphertext.encode())
+    print(decrypted.decode())
 
 
+# Encrypt the message
+encrypted_message = fernet.encrypt(message.encode())
+message ="The Princess is in another castle."
+
+# Write the encrypted message to a file
+with open("encrypted_message.txt", "wb") as f:
+    f.write(encrypted_message)
+
+# Read the encrypted message from the file
+with open("encrypted_message.txt", "rb") as f:
+    encrypted_message = f.read()
+
+# Decrypt the message
+decrypted_message = fernet.decrypt(encrypted_message)
+
+## main Part
 
 
+def main():
+    print("Select a mode:")
+    print("1. Encrypt a file")
+    print("2. Decrypt a file")
+    print("3. Encrypt a message")
+    print("4. Decrypt a message")
 
+    mode = int(input("Enter mode: "))
+    key = Fernet.generate_key()
 
+    if mode == 1:
+        filepath = input("Enter filepath: ")
+        encrypt_file(key, filepath)
+    elif mode == 2:
+        filepath = input("Enter filepath: ")
+        decrypt_file(key, filepath)
+    elif mode == 3:
+        plaintext = input("Enter plaintext: ")
+        encrypt_string(key, plaintext)
+    elif mode == 4:
+        ciphertext = input("Enter ciphertext: ")
+        decrypt_string(key, ciphertext)
+    else:
+        print("Invalid mode selected.")
 
-
-
-# Main Part
-
-write_key()
-
-load_key
-
-
-
-
-
-
-
-
-
-# Encrypt
-message = "The princess is in another castle."
-
-
-print("Plain text is " + str(message))
-
-print()
-
+if __name__ == "__main__":
+    main()
